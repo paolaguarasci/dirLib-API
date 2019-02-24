07ccc8d70d4cee396dd7940ab03dd96ce0cd7fff
Paola Guarasci <paolaguarasci@gmail.com>
2019-02-24 17:27:02 +0100

    User auth

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import Promise from "bluebird";
+
+import auth from "./routes/auth";
 
 const app = express();
+app.use(bodyParser.json());
 
-app.post("/api/auth", (req, res) => {
-  res.status(400).json({ errors: { global: "Invalid credentials" } });
+const port = process.env.PORT;
+mongoose.Promise = Promise;
+mongoose.connect(process.env.DBLINK, {
+  useNewUrlParser: true
 });
+
+app.use("/api/auth", auth);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(8080, () => console.log("Running on localhost:8080"));
+app.listen(port, () => console.log(`Running on localhost: ${port}.`));
